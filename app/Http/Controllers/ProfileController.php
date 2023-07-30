d7a9467abb4f831c81b302d39ec6166b67b1d5bd
reyad-abdalbari <140594502+reyad-abdalbari@users.noreply.github.com>
2023-07-31 01:53:50 +0300

    Test-Laravel-Migrations

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ProfileUpdateRequest;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class ProfileController extends Controller
+{
+    public function show()
+    {
+        return view('auth.profile');
+    }
+
+    public function update(ProfileUpdateRequest $request)
+    {
+        // Task: fill in the code here to update name and email
+        // Also, update the password if it is set
+        $user = Auth::user();
+
+        $user->update($request->only('name','email'));
+
+        if ($request->password) {
+            $user->update(['password' => Hash::make($request->password)]);
+        }
+
+        
+
+        return redirect()->route('profile.show')->with('success', 'Profile updated.');
+    }
+}
